80275a71788dcfb288deac641bce0980f398ce9a
kyun <kyun9911@gmail.com>
2025-03-11 20:03:40 +0900

    페이먼트서비스 테스트 커밋2

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -9,26 +9,35 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentServiceTest {
     // 해당하는 기존 코드에서 테스트를 제네레이트 할 수 있다.
     @Test
     @DisplayName("prepare 메서드가 요구사항 3가지를 잘 충족했는지 검증")
-    void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
-
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
-
-        // 환율정보 가져온다.
-        Assertions.assertThat(payment.getExRate()).isNotNull();
-
-        // 원화환산금액 계산
-        Assertions.assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
 
         // 원화환산금액의 유효시간 계산
-        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        Assertions.assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        //assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+        //assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+    /* 외부의 api를 제어하는 테스트는 외부 환경 변경 등이 있을 때 검증에 위험하다.
+        그래서 외부 api를 대체하는 인터페이스(임포스터, 스텁 등으로 부른다.)를 만들어 대체하여 테스트한다.
+     */
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate)); // stub에서 외부에서 값을 가져왔다고 가정하고 특정한 값을 제공한다.
+
+        Payment payment = paymentService.prepare(1L, "USD", TEN);
+
+        // 환율정보 가져온다.
+        assertThat(payment.getExRate()).isEqualTo(exRate);
+        // 원화환산금액 계산
+        assertThat(payment.getConvertedAmount()).isEqualTo(convertedAmount); // 자리수 구분용으로 _를 사용해도 된다.
     }
 }
