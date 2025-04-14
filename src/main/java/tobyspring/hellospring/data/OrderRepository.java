f8ddcbd81723f0367dfc3f5c747205b69533dfa7
kyun <kyun9911@gmail.com>
2025-04-14 21:40:47 +0900

    DataAccessException과 예외 추상화 - db 종류와 기술에 상관없이 일관된 예외처리를 할 수 있다.

--- src/main/java/tobyspring/hellospring/data/OrderRepository.java
+++ src/main/java/tobyspring/hellospring/data/OrderRepository.java
@@ -3,34 +3,16 @@ package tobyspring.hellospring.data;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
 
 public class OrderRepository {
-    private final EntityManagerFactory emf;
-
-    public OrderRepository(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public void save(Order order) {
-        // em
-        EntityManager em = emf.createEntityManager();
-
-        // transaction
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-
-        // em.persist
-        try {
-            em.persist(order);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if(transaction.isActive()) transaction.rollback();
-            throw e;
-        } finally {
-            if(em.isOpen()) em.close();
-        }
+        entityManager.persist(order);
     }
 }
